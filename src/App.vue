27f79ff6e9971952b3746d657788f467fb5515f8
asreichardt <adriano.cobhc2@gmail.com>
2021-12-03 12:12:13 -0300

    add nav dinamica

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,9 @@
           </ul>
       </nav>
 
-      <router-view></router-view>
-            
+    <transition name="pagina">
+          <router-view></router-view>
+    </transition>      
   </div>
 </template>
 
@@ -35,5 +36,18 @@ export default {
   width: 96%;
   margin: 0 auto;
 }
+
+/**    EFEITOS DE TRANSIÇÃO, FUNCIONAM COM A TAG <TRANSITION)
+    painel-fade-enter // antes do elemento ser incluído ou removido, o estado atual
+     painel-fade-enter-active // quando o elemento esta sendo incluído
+     painel-fade-leave-active // quando o elemento esta sendo removido */ 
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
+  }
+
+.pagina-enter-active, .pagina-leave-active {
+  transition: opacity .4s
+}
+
 
 </style>
